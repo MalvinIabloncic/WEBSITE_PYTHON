8c283f8db7b7ad54a5f19c754d726aa77788beba
MalvinIabloncic <115619312+MalvinIabloncic@users.noreply.github.com>
2023-02-15 17:40:08 +0200

    Update __init__.py

--- WEBSITE_PYTHON/Website/__init__.py
+++ WEBSITE_PYTHON/Website/__init__.py
@@ -8,7 +8,7 @@ DB_NAME="database.db"
 
 def create_app():
     app=Flask(__name__)
-    app.config['SECRET_KEY']='SECRET DISCRET'
+    app.config['SECRET_KEY']='secret key'
     app.config['SQLALCHEMY_DATABASE_URI']=f'sqlite:///{DB_NAME}'
     db.init_app(app)
     
